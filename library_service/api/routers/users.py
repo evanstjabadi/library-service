e7a6fec54f6a947397aea874d3b6bef1595ad256
evanstjabadi <evansltjabadi@gmail.com>
2022-01-13 12:24:05 +0200

    add and retrieve user

--- library_service/api/routers/users.py
+++ library_service/api/routers/users.py
@@ -1,14 +1,29 @@
 from typing import Dict
 
-from fastapi import APIRouter
+from fastapi import APIRouter,Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
 
 from library_service.controllers.users import Controller
+from library_service.models.users import Base
+from library_service.schemas.users import Schemas
+from library_service.models.db import session, engine
+
+Base.metadata.create_all(bind=engine)
 
 controller = Controller()
+schemas = Schemas()
 
 router = APIRouter()
 
-@router.get("/users")
+
+def get_db():
+    db = session()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.get("/users/home")
 async def user_root():
     try:
         result = controller.get_root()
@@ -25,16 +40,26 @@ async def user_root():
             "error": str(e)
         }
 
-@router.post("/users")
-async def add_user(user: Dict):
-    try:
-        result = await controller.add_user(user)
-        return {
-            "status": "success",
-            "data": result
-        }
-    except Exception as e:
-        return {
-            "status": "error",
-            "error": str(e)
-    }
+
+@router.post("/users/", response_model=schemas.User)
+async def add_user(user: schemas.UserCreate, db: Session = Depends(get_db))->schemas.User:
+    db_user = controller.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return controller.create_user(db=db, user=user)
+
+
+@router.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)) -> schemas.User:
+    db_user = controller.get_user(db=db,user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+
+@router.get('/users/email/{user_email}', response_model=schemas.User)
+def read_user_by_email(user_email: str, db: Session = Depends(get_db)) -> schemas.User:
+    db_user = controller.get_user_by_email(db, email=user_email)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found by email")
+    return db_user  
--- library_service/models/users.py
+++ library_service/models/users.py
@@ -1,9 +1,13 @@
-from typing import Optional
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 
-from pydantic import BaseModel
+from library_service.models.db import Base
 
-class User(BaseModel):
-    id: int
-    name: str
-    email: str
-    role: str
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    email = Column(String(120), nullable=False)
+    password = Column(String(50), nullable=False)
+    role = Column(String(50), nullable=False, default='reader') # reader, librarian, author, super
